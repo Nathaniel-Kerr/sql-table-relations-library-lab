1ae43835875e26360aabafdffeea69faeb3ec4f5
Nathaniel-Kerr <NathanielKerr10@gmail.com>
2020-12-04 15:25:55 -0800

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,44 @@
+INSERT INTO series(title, author_id, subgenre_id) VALUES('Night Angle', 1, 1);
+INSERT INTO series(title, author_id, subgenre_id) VALUES('Night Walker', 2, 2);
+
+INSERT INTO books(title, year, series_id) VALUES('The Way of Shadows', 2012, 1);
+INSERT INTO books(title, year, series_id) VALUES('Shadows Edge', 2015, 1);
+INSERT INTO books(title, year, series_id) VALUES('Left 4 Dead', 2005, 2);
+INSERT INTO books(title, year, series_id) VALUES('Dying Light', 2010, 3);
+INSERT INTO books(title, year, series_id) VALUES('Left 4 Dead', 2005, 2);
+INSERT INTO books(title, year, series_id) VALUES('Left 4 Dead', 2005, 2);
+
+INSERT INTO characters(name, motto, species, author_id) VALUES('Azoth', 'gutter shite', 'guild rat', 1);
+INSERT INTO characters(name, motto, species, author_id) VALUES('Durzo Blint', 'Life is empty. Life is meaningless. When we take a life, we aren’t taking anything of value', 'Wetboy', 1);
+INSERT INTO characters(name, motto, species, author_id) Values('Kylar', NULL, 'Wetboy', 1);
+INSERT INTO characters(name, motto, species, author_id) Values('Sam', 'Get that dew licker', 'Human', 2);
+INSERT INTO characters(name, motto, species, author_id) Values('Kreg', 'Get away from me ya filthy anaimal', 'Human', 2);
+INSERT INTO characters(name, motto, species, author_id) Values('Kyle Crane', 'Goodnight, Good Luck', 'Human', 3);
+INSERT INTO characters(name, motto, species, author_id) Values('Kyle Crane', 'Goodnight, Good Luck', 'Human', 3);
+INSERT INTO characters(name, motto, species, author_id) Values('Kyle Crane', 'Goodnight, Good Luck', 'Human', 3);
+
+INSERT INTO subgenres(name) VALUES('Zombies');
+INSERT INTO subgenres(name) VALUES('medieval');
+
+INSERT INTO authors(name) VALUES('Brent Weeks');
+INSERT INTO authors(name) VALUES('Stuart MacBride');
+
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (2, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (3, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+
+
+
